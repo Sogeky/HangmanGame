c6afd3055910cc93a27dd9f7dcff76a7b56c5d25
Leandro Gomes <isogeky@gmail.com>
2021-09-05 02:49:29 +0100

    Update app.js
    finished the logic of the images display 1 by 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const word = prompt("select the word for your guesser");
 const sw = document.querySelector(".sw");
 
 //Inicio. Colocar o boneco 'invisível'
-const bonecoPartes = document.querySelectorAll(".boneco");
+let bonecoPartes = document.querySelectorAll(".boneco");
 console.log(bonecoPartes);
 
 //Coloca a Palavra em asteriscos
@@ -22,15 +22,28 @@ let guess = document.querySelector(".box");
 let clickBox = document.querySelector(".click");
 
 //Lógica do jogo,Evento de quando clicar no BTN e verifica se a palavra está correta ou não
+let nextImg = [...bonecoPartes]; //Transformar em array e dar distruct nas imgs
 
+let imageUp = 0; // mecanismo para fazer as imagens subirem de 0 a 6 -> vemos isto acontecendo na function
+
+const imgNext = function () {
+  if (imageUp < bonecoPartes.length) {
+    // se 0 (imageUp) for menor que 6 (bonecoPartes.lenght)
+    imageUp++; // imageUp vai somar 1 cada vez que apertar no btn
+    nextImg.slice(0, imageUp); // Slice() vai fazer a img começar em 0 e imageUp vai dar increase e sempre que apretar o btn, ou seja, cada imagem vai aparecendo 1 a 1
+    console.log(nextImg.slice(0, imageUp)); // sera que funciona? SIMMMMM !!!!!!!
+  }
+};
+
+//Não estava a conseguir colocar o conteudo da function a cima dentro do evento, então tive que criar uma função.
 clickBox.addEventListener("click", function () {
   if (guess.value != word) {
-    for (let i = 0; i < bonecoPartes.length; i++) {
-      bonecoPartes[i];
-      console.log(bonecoPartes[i]);
-    }
+    imgNext();
+  } else {
+    console.log("correto");
   }
 });
+
 //  ^^^^^ ACABAR LOGICA ^^^^^^
 
 //Reset Do Jogo
